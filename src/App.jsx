cb8f271b0b2ac0fcdfd9c5df4ba46edb6f49abcd
HimaSaiCharan <127401464+HimaSaiCharan@users.noreply.github.com>
2025-05-04 16:57:43 +0530

    now the list item toggles

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,68 @@ import { Component } from "react";
 class Task extends Component {
   constructor(props) {
     super(props);
+    this.toggle = () => this.props.toggle(this.props.task.taskId);
   }
 
   render() {
-    return <li>{this.props.task}</li>;
+    return (
+      <li
+        style={{
+          textDecoration: this.props.task.done ? "line-through" : "none",
+        }}
+        onClick={this.toggle}
+      >
+        {this.props.task.description}
+      </li>
+    );
+  }
+}
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { value: "" };
+  }
+
+  handleChange(event) {
+    this.setState((prev) => {
+      return { value: event.target.value };
+    });
+  }
+
+  render() {
+    return <input type="text" onChange={this.handleChange} />;
   }
 }
 
 class TaskList extends Component {
   constructor(props) {
     super(props);
+    this.state = { value: "", tasks: this.props.tasks };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(taskId) {
+    this.setState((prev) => {
+      const tasks = prev.tasks.map((task) => {
+        return task.taskId === taskId ? { ...task, done: !task.done } : task;
+      });
+
+      return { ...prev, tasks };
+    });
   }
 
   render() {
     return (
-      <ul>
-        {this.props.tasks.map((task) => {
-          return <Task task={task} />;
-        })}
-      </ul>
+      <div>
+        <Input />
+        <ul>
+          {this.state.tasks.map((task) => {
+            return <Task key={task.taskId} task={task} toggle={this.toggle} />;
+          })}
+        </ul>
+      </div>
     );
   }
 }
@@ -30,7 +73,12 @@ function App() {
   return (
     <>
       <div>
-        <TaskList tasks={["buy milk", "buy water"]} />
+        <TaskList
+          tasks={[
+            { description: "buy milk", taskId: 1, done: false },
+            { description: "buy water", taskId: 2, done: false },
+          ]}
+        />
       </div>
     </>
   );
